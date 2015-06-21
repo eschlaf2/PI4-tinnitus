8e238b1e924a6073363c7884c811243d011def08
eschlaf2 <eschlaf2@users.noreply.github.com>
2015-06-21 14:27:48 -0500

    Create plot_cyclicdist_v_noise.m
    Plots performance of methods over increasing noise interference. All of the functions it uses are below.

---
+++ plot_cyclicdist_v_noise.m
@@ -0,0 +1,30 @@
+% Compare performance of two methods of extracting cycles 
+noise_magnitude = (0:.1:3);
+rois = 6; loops = 6; time = 100; 
+trials = 100;
+types = {'sines', 'altered_sines', 'non-cyclic','step','gaussian', 'data'};
+
+% set display_ to non-zero to show plots of methods during analysis.
+% Probably only do this for single values of types and noise_magnitude.
+display_ = 0; 
+
+fft_dist = zeros(size(noise_magnitude)); eig_dist = fft_dist;
+
+for t = 1:length(types)
+    type = types(t);
+    for n = 1:length(noise_magnitude)
+        for k = 1:trials
+            [f, e] = compare_methods(rois, loops, time, noise_magnitude(n), type, display_);
+            fft_dist(n) = fft_dist(n) + f;
+            eig_dist(n) = eig_dist(n) + e;
+        end
+        fft_dist(n) = fft_dist(n)/trials; eig_dist(n) = eig_dist(n)/trials;
+    end
+    figure
+    subplot(ceil(length(types)/2),2,t)
+    plot(noise_magnitude, fft_dist, 'b', noise_magnitude, eig_dist, 'r');
+    title(type, 'FontWeight', 'bold');
+    legend('fft', 'eig', 'Location', 'southeast')
+    xlabel('Noise magnitude')
+    ylabel('Cyclic distance')
+end
