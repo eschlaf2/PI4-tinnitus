c298a1cc976518303814840f61880a124624f838
eschlaf2 <eschlaf2@users.noreply.github.com>
2015-06-21 18:08:18 -0500

    Update plot_cyclicdist_v_noise.m
    Updated to give the figure a specific name for easy reference and in case any sub functions produce figures (i.e. if display_ is set to non-zero).

--- plot_cyclicdist_v_noise.m
+++ plot_cyclicdist_v_noise.m
@@ -3,15 +3,17 @@ noise_magnitude = (0:.1:3);
 rois = 6; loops = 6; time = 100; 
 trials = 100;
 types = {'sines', 'altered_sines', 'non-cyclic','step','gaussian', 'data'};
-
 % set display_ to non-zero to show plots of methods during analysis.
 % Probably only do this for single values of types and noise_magnitude.
 display_ = 0; 
 
 fft_dist = zeros(size(noise_magnitude)); eig_dist = fft_dist;
 
+cdist_plot = figure();
+% subplot dimensions 
+p = floor(sqrt(numel(types))); q = ceil(numel(types)/p);
 for t = 1:length(types)
-    type = types(t);
+    type = types{t};
     for n = 1:length(noise_magnitude)
         for k = 1:trials
             [f, e] = compare_methods(rois, loops, time, noise_magnitude(n), type, display_);
@@ -20,8 +22,9 @@ for t = 1:length(types)
         end
         fft_dist(n) = fft_dist(n)/trials; eig_dist(n) = eig_dist(n)/trials;
     end
-    figure
-    subplot(ceil(length(types)/2),2,t)
+    
+    figure(cdist_plot)
+    subplot(p, q, t)
     plot(noise_magnitude, fft_dist, 'b', noise_magnitude, eig_dist, 'r');
     title(type, 'FontWeight', 'bold');
     legend('fft', 'eig', 'Location', 'southeast')
