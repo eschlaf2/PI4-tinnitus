590b2e7a6f5744bf73e3344536eb703c73176313
Emily <eschlaf2@illinois.edu>
2015-10-06 14:24:24 -0500

    sort_lead outputs complex eigenvectors rather than real phase angles

--- sort_lead.m
+++ sort_lead.m
@@ -1,4 +1,4 @@
-function [phases, permutation, sorted_lead_matrix, spectrum]=sort_lead(a, varargin)
+function [eig_vec, permutation, sorted_lead_matrix, spectrum]=sort_lead(a, varargin)
 % The first input should be the matrix to be sorted, the second should be
 % the title of an ROI to be used as a baseline.
 % On July 2 I changed the output so that phases outputs an angle rather
@@ -7,18 +7,18 @@ function [phases, permutation, sorted_lead_matrix, spectrum]=sort_lead(a, vararg
 % baseline.
 
 
-   	 [u,spectrum]=eig(a);
+   	 [eig_vec,spectrum]=eig(a);
      spectrum = diag(spectrum);
-     sorted_ang = sort(mod(angle(u(:,1)),2*pi));
+     sorted_ang = sort(mod(angle(eig_vec(:,1)),2*pi));
      [~, shift] = max(abs(diff([sorted_ang; sorted_ang(1) + 2*pi])));
-     if shift == numel(u(:,1))
+     if shift == numel(eig_vec(:,1))
          shift = 1; 
      else
          shift = shift + 1;
      end
      shift = sorted_ang(shift);
 %      shift = pi - median(mod(angle(u(:,1).'), 2*pi));
-	 [phases,permutation]=sort(mod(mod(angle(u(:,1).'), 2*pi) - shift, 2*pi));
+	 [phases,permutation]=sort(mod(mod(angle(eig_vec(:,1).'), 2*pi) - shift, 2*pi));
           
 %      Phase adjust
 switch nargin
@@ -42,4 +42,5 @@ end
 
      phases = phases - ones(size(phases)) .* angle_adjust;
 	 sorted_lead_matrix=a(permutation,permutation);
+     eig_vec = eig_vec(permutation,:);
 end
