4aa1c3f3f261a16632383084e3491b9b1fafa6e2
Emily <eschlaf2@illinois.edu>
2016-01-16 20:14:26 -0600

    minor changes

--- analyze_cyclicity.m
+++ analyze_cyclicity.m
@@ -1,15 +1,17 @@
 % Returns cyclicity results for processed subject data in current
 % directory. Saves results in 'cyclicity.mat'.
 files = dir('processed*');
-n = length(files);
-phases = cell(1,n); perms = cell(1,n); slm = cell(1,n); evals = cell(1,n); 
-subject = cell(1,n);
-i = 1;
-for file = files.'
-	data_file = file.name;
-    sbj = strsplit(file.name,{'_','.'},'Collapsedelimiters',true);
-    subject{i} = sbj{3};
-	[phases{i}, perms{i}, slm{i}, evals{i}] = cyclic_analysis(data_file);
-	i = i + 1;
+for norm_method = {'z-score','frob'}
+	n = length(files);
+	phases = cell(1,n); perms = cell(1,n); slm = cell(1,n); evals = cell(1,n); 
+	subject = cell(1,n);
+	i = 1;
+	for file = files.'
+		data_file = file.name;
+	    sbj = strsplit(file.name,{'_','.'},'Collapsedelimiters',true);
+	    subject{i} = sbj{3};
+		[phases{i}, perms{i}, slm{i}, evals{i}] = cyclic_analysis(data_file, norm_method{:});
+		i = i + 1;
+	end
+	save(['cyclicity_',norm_method{:},'.mat'], 'phases', 'perms','slm','evals','subject');
 end
-save('cyclicity.mat', 'phases', 'perms','slm','evals','subject');
